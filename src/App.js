0fb07f9050103ea9a41d05385e9a57b93d92401a
intae kim <intaemon@intaeui-MacBookAir.local>
2021-09-11 15:05:37 +0900

    feat: landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import AppLayout from '@/components/layouts/AppLayout';
 import Header from '@/components/Header';
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,18 @@
 import { useHistory } from 'react-router-dom';
 import Button from '@/components/elements/Button';
 
+import main from '@/img/main.png';
+
 export default function Landing() {
   const history = useHistory();
   const goBoard = () => history.push('/board');
   return (
-    <div>
-      <h1>hello, Tailwind ToDo app</h1>
-      <Button onClick={goBoard}>start</Button>
+    <div className="flex flex-col justify-center h-full">
+      <img className="w-full h-1/2 object-contain" src={main} alt="main logo" />
+      <h1 className="text-center text-lg mb-5">hello, Tailwind ToDo app</h1>
+      <Button onClick={goBoard} className="w-full">
+        start
+      </Button>
     </div>
   );
 }
